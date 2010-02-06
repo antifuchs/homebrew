9501b3634eb1f9f379b05d90efeca237423ee9eb
Andreas Fuchs <asf@boinkor.net>
2010-02-06 14:39:17 +0100

    Don't install postgis/postgis_upgrade.sql, as it doesn't exist.

--- Library/Formula/postgis.rb
+++ Library/Formula/postgis.rb
@@ -14,7 +14,7 @@ class Postgis <Formula
     system "make install"
 
     # Copy some of the generated files to the share folder
-    (share+'postgis').install %w( spatial_ref_sys.sql postgis/postgis.sql postgis/postgis_upgrade.sql postgis/uninstall_postgis.sql )
+    (share+'postgis').install %w( spatial_ref_sys.sql postgis/postgis.sql postgis/uninstall_postgis.sql )
     # Copy loader and utils binaries to bin folder
     bin.install %w( loader/pgsql2shp loader/shp2pgsql utils/new_postgis_restore.pl utils/postgis_proc_upgrade.pl utils/postgis_restore.pl utils/profile_intersects.pl )
 
