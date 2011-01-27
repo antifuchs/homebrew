241b42a1317dfc9d4557742b322cbbe7eabee929
Andreas Fuchs <asf@boinkor.net>
2011-01-27 15:35:05 -0800

    Add openmotif 2.3.3
    This builds the OpenMotif library as a universal binary.
    Conflicts:
    
    	Library/Formula/openmotif.rb

--- Library/Formula/openmotif.rb
+++ Library/Formula/openmotif.rb
@@ -1,17 +1,22 @@
 require 'formula'
 
 class Openmotif <Formula
-  url 'http://www.motifzone.net/files/public_downloads/openmotif/2.3/2.3.2/openmotif-2.3.2.tar.gz'
-  homepage 'http://www.motifzone.net'
-  md5 ''
+  url 'http://motifzone.com/files/public_downloads/openmotif/2.3/2.3.3/openmotif-2.3.3.tar.gz'
+  homepage 'http://motifzone.com'
+  md5 'fd27cd3369d6c7d5ef79eccba524f7be'
 
-# depends_on 'cmake'
+  def patches
+    # Allow UIL to compile on OS X - allegedly fixed in the release after 2.3.3:
+    {:p1 => 'http://bugs.motifzone.net/showattachment.cgi?attach_id=259' }
+  end
 
   def install
-    ENV.universal_binary   # Many motif-using programs don't like 64-bit mode.
-    
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
-#   system "cmake . #{std_cmake_parameters}"
+    ENV.universal_binary
+    ENV.j1
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-jpeg", "--enable-png", "--enable-xft"
+    system "make"
     system "make install"
   end
 end
